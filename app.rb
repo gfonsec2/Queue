fa2e1b7b7c939134001d8c94910dccc05e87a85e
Gilbert <gibbyfonseca74@gmail.com>
2019-06-19 03:19:21 -0500

    appointments almost finished

--- app.rb
+++ app.rb
@@ -22,11 +22,28 @@ class Barber
 	property :total, Integer, :default => 0
 	property :available, Boolean, :default => true
 	property :money, Integer, :default => 0
+
 	#fill in the rest
 	def wait_list
 		return Queueitem.all(bid: id) #gets list of customers 
 	end
 	
+end
+
+class Appointment
+	include DataMapper::Resource
+	property :id, Serial
+	property :shopID, Integer, :default => 0
+	property :shopName, Text
+	property :barberID, Integer, :default => 0
+	property :weekDay, Integer, :default => 0
+	property :day, Text
+	property :time, Integer, :default => 0
+	property :hairID, Integer
+	property :beardID, Integer
+	property :extraID, Integer
+	property :cost, Integer
+	property :name, Text
 end
 
 class Queueitem
@@ -77,6 +94,7 @@ Queueitem.auto_upgrade!
 Date.auto_upgrade!
 Haircuts.auto_upgrade!
 Extra.auto_upgrade!
+Appointment.auto_upgrade!
 
 # Perform basic sanity checks and initialize all relationships
 # Call this when you've defined all your models
--- appointment_functions.rb
+++ appointment_functions.rb
@@ -4,13 +4,111 @@ get "/aboutUs" do
 	erb :aboutUs
 end
 get "/appointments/barbershop" do
+	app = Appointment.new
+	app.save
+	@appointment = app.id
+	erb :apptBarberShop
+end
+
+post "/appointments/stylistChoice/:id" do
 	@barbers = Barber.all
+	currentAppt = Appointment.get(params[:id])
+	@id = params[:id]
+	currentAppt.shopName = params["barberShop"]
+	currentAppt.save
+	@shopName = currentAppt.shopName
+	erb :apptStylistChoice
+end
+
+get "/appointments/stylistAvailability/:id/:bid" do
+	currentAppt = Appointment.get(params[:id])
+	currentAppt.barberID = params[:bid]
+	currentAppt.save
+	@id = params[:id]
+	@barberName = Barber.get(currentAppt.barberID)
+	erb :apptDayChoice
+end
+
+post "/appointments/haircuts/:id" do
 	@hairtypes = Haircuts.all(hair: true)
 	@beardtypes = Haircuts.all(hair: false)
 	@extra = Extra.all
-	erb :appointments
+
+	currentAppt = Appointment.get(params[:id])
+	currentAppt.day = params["weekDay"]
+	currentAppt.time = params["time"]
+	@id = params[:id]
+	currentAppt.save
+	@shopName = currentAppt.shopName
+	erb :apptHaircut
 end
-get "/appointments/dayChoice" do
+
+post "/appointments/payment/:id" do
+	currentAppt = Appointment.get(params[:id])
+	currentAppt.hairID = params["hairtype"]
+	currentAppt.beardID = params["beardtype"]
+	currentAppt.extraID = params["extratype"]
+	currentAppt.save
+	@cost = 0
+	if (params["hairtype"] != "0")
+		@hairtype = Haircuts.get(currentAppt.hairID)
+		@hairname = @hairtype.hair_type
+		@cost += @hairtype.price
+	end
+	if (params["beardtype"] != "0")
+		@beardtype = Haircuts.get(currentAppt.beardID)
+		@beardname = @beardtype.hair_type
+		@cost += @beardtype.price
+	end
+	if (params["extratype"] != "0")
+		@extratype = Extra.get(currentAppt.extraID)
+		@extraname = @extratype.name
+		@cost += @extratype.price
+	end
+	currentAppt.cost = @cost
+	currentAppt.save
+
+	@id = params[:id]
+	erb :apptFinalize
+end
+
+post "/appointments/confirmation/:id" do
+	currentAppt = Appointment.get(params[:id])
+	currentAppt.name = params["pname"]
+	currentAppt.save
+	@barber = Barber.get(currentAppt.barberID)
+	@hairname = @beardname = @extraname = "None"
+	if (currentAppt.hairID != 0)
+		hairtype = Haircuts.get(currentAppt.hairID)
+		@hairname = hairtype.hair_type
+	end
+	if (currentAppt.beardID != 0)
+		beardtype = Haircuts.get(currentAppt.beardID)
+		@beardname = beardtype.hair_type
+	end
+	if (currentAppt.extraID != 0)
+		extratype = Extra.get(currentAppt.extraID)
+		@extraname = extratype.name
+	end
+
+	@name = currentAppt.name
+	@shopName = currentAppt.shopName
+	@day = currentAppt.day
+	@time = currentAppt.time
+	@barberName = @barber.name
+	@cost = currentAppt.cost
+
+	erb :apptSuccessMessage
+end
+
+
+
+
+
+
+
+get "/appointments/:id/dayChoice" do
+	app = appointments.get(params[:id])
 	@hairname = @beardname = @extraname = "N/A"
 	@shop = params["barberShop"]
 	@day = params["dayChoice"]
